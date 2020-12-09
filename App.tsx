9b8aaa7db79b0547ca8b05d45a8f99428be10512
Vinh Duc Hoang <vinh@streethawk.com>
2020-12-09 17:46:37 +0700

    integrate web sdk

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import PointziReact from 'pointzi-react'
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -13,6 +14,12 @@ export default function App() {
   if (!isLoadingComplete) {
     return null;
   } else {
+    PointziReact.ready(()=>{
+      PointziReact.register("cordova_conference");
+    })
+
+    console.log('render app.tsx!');
+
     return (
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
